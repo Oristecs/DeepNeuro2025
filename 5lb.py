4d6210e367d0d036439ba90545b689b87e1cf1ca
Пронькин Сергей 23ВЭ1 <147483705+Oristecs@users.noreply.github.com>
2025-04-23 14:18:56 +0300

    Update 5lb.py

--- 5lb.py
+++ 5lb.py
@@ -47,46 +47,10 @@ class_names = train_dataset.classes
 inputs, classes = next(iter(train_loader))
 inputs.shape 
 
-# Сеть
-class CnNet(nn.Module):
-    def __init__(self, num_classes=10):
-        nn.Module.__init__(self)
-        self.layer1 = nn.Sequential(
-        # первый сверточный слой с ReLU активацией и maxpooling-ом
-            nn.Conv2d(3, 16, kernel_size=7, stride=1, padding=2), # 3 канала, 16 фильтров, размер ядра 7
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        # второй сверточный слой 
-        # количество каналов второго слоя равно количеству фильтров предыдущего слоя
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        # третий сверточный слой 
-        # ядро фильтра от слоя к слою уменьшается
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=2),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        # классификационный слой имеет нейронов: количество фильтров * размеры карты признаков
-        self.fc = nn.Linear(8*8*64, num_classes)
-        
-    def forward(self, x):
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = out.reshape(out.size(0), -1) # флаттеринг
-        out = self.fc(out)
-        return out
-
-
-
 # Количество классов
 num_classes = 3
 
 # создаем экземпляр сети
-net = CnNet(num_classes).to(device)
-
 net = torchvision.models.alexnet(pretrained=True)
 
 for param in net.parameters():
